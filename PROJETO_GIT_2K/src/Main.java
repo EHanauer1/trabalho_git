55700a009f02ab1ad6b06b8f15031214caa88f29
Erick Andrei Hanauer Machado <erickmachado.ss031@academico.ifsul.edu.br>
2023-02-23 08:31:49 -0300

    Adicionado print Olá dev

--- PROJETO_GIT_2K/src/Main.java
+++ PROJETO_GIT_2K/src/Main.java
@@ -4,11 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-            Scanner t = new Scanner(System.in);
-    		System.out.println("Digite o seu nome: ");
-    		String a = t.nextLine();
-    		System.out.println("Olá " + a);
     		System.out.println("Olá Mundo");
+    		System.out.println("Olá dev!");
     		
 	}
 
